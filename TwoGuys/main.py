1b058bd0b874e929e763d6432dd067aacfa7e312
Mosucule <silpos03@yahoo.com>
2024-11-11 17:00:57 +0200

    Formatare un set de date

--- TwoGuys/main.py
+++ TwoGuys/main.py
@@ -1,7 +1,32 @@
+import pandas as pd
+import os
 import numpy as np
 
 def main():
-    print("Hello Word!")
+    meta_path = "../data/metadata/training_metadata.csv"
+    file = "sub-NDARAA306NT2_ses-HBNsiteRU_task-rest_run-2_atlas-Schaefer2018p200n17_space-MNI152NLin6ASym_reg-36Parameter_desc-PearsonNilearn_correlations.tsv"
+    metadata = pd.read_csv(meta_path)
+    format(file, metadata)
+
+
+def format(file, metadata):
+    dir_path = "../data/"
+
+    mri = pd.read_csv(dir_path + "train_tsv/" + file, sep='\t', header=None)
+    ID = file[4:16]
+
+    metadata.set_index('participant_id', inplace=True)
+    data = metadata.loc[ID]
+
+    upper_indices = np.triu_indices(mri.shape[0], k=1)
+    upper_data = mri.values[upper_indices]
+
+    mri_data = pd.Series(upper_data)
+    data = pd.concat([data, mri_data])
+
+    print(data)
+
+
 
 
 if __name__ == '__main__':
