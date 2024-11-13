8c2a7732c5f42e742ab9ce69aedebee1582de10a
Mosucule <silpos03@yahoo.com>
2024-11-13 23:43:34 +0200

    adaugare filtrare date

--- TwoGuys/func.py
+++ TwoGuys/func.py
@@ -10,6 +10,53 @@ def read_metadata(meta_path):
     return metadata
 
 
+def write_metadata(meta_path, metadata: pd.DataFrame):
+    metadata.to_csv(meta_path)
+
+
+def fill_metadata(metadata: pd.DataFrame):
+    prob = metadata[['ethnicity', 'race']].value_counts(normalize=True)
+    columns_data = metadata[['ethnicity', 'race']].copy()
+    temp = columns_data.isna().values
+    col1e = temp[:, 0]
+    col2e = temp[:, 1]
+    val_completat = np.random.choice(prob.index, p=prob.values, size=(col1e & col2e).sum())
+    val_completat = list(list(x) for x in val_completat)
+    columns_data[col1e & col2e] = val_completat
+    #metadata[['ethnicity', 'race']] = columns_data
+
+    col1 = 'ethnicity'
+    col2 = 'race'
+    for i in range(2):
+        for valoare_col in columns_data[col1].unique():
+            if pd.isna(valoare_col):
+                continue
+            subset = columns_data[columns_data[col1] == valoare_col]
+
+            distributie = subset[col2].value_counts(normalize=True)
+
+            while sum(distributie) < 1.0:
+                distributie /= distributie.sum()
+
+            valori_umplere = np.random.choice(distributie.index, p=distributie.values, size=subset[col2].isna().sum())
+
+            columns_data.loc[(columns_data[col1] == valoare_col) & (columns_data[col2].isna()), col2] = valori_umplere
+        col1 = 'race'
+        col2 = 'ethnicity'
+
+    metadata[[col2, col1]] = columns_data
+
+    for column in metadata.columns:
+        if metadata[column].isna().values.any():
+            prob = metadata[column].value_counts(normalize=True)
+            column_data = metadata[column].copy()
+            val_completat = np.random.choice(prob.index, p=prob.values, size=column_data.isna().sum())
+            column_data[column_data.isna()] = val_completat
+            metadata[column] = column_data
+
+    return metadata
+
+
 def format(file, metadata):
     dir_path = "../data/"
 
@@ -27,7 +74,7 @@ def format(file, metadata):
     return data
 
 
-def show_statistics(metadata: pd.DataFrame):
+def show_statistics(metadata: pd.DataFrame, hist=False):
     missing = metadata.isna().mean() * 100
     no_people_valid = metadata.notna().all(axis=1).mean() * 100
     no_people_1NA = (metadata.isna().sum(axis=1) <= 1).mean() * 100
@@ -36,12 +83,13 @@ def show_statistics(metadata: pd.DataFrame):
     print(f"People with all data: {no_people_valid:.2f}%")
     print(f"People with maximum one data missing: {no_people_1NA:.2f}%")
 
-    for column in metadata.columns:
-        metadata[column] = metadata[column].fillna(metadata[column].mode()[0])
-        plt.figure(figsize=(8, 6))
-        sns.histplot(metadata[column], bins=10, kde=False)
-        plt.title(f'Histograma pentru coloana {column}')
-        plt.show()
+    if hist:
+        for column in metadata.columns:
+            metadata[column] = metadata[column].fillna(metadata[column].mode()[0])
+            plt.figure(figsize=(8, 6))
+            sns.histplot(metadata[column], bins=10, kde=False)
+            plt.title(f'Histograma pentru coloana {column}')
+            plt.show()
 
 
 
--- TwoGuys/main.py
+++ TwoGuys/main.py
@@ -6,6 +6,12 @@ from func import *
 def main():
 
     metadata = read_metadata("../data/metadata/training_metadata.csv")
+    show_statistics(metadata)
+
+    metadata = fill_metadata(metadata)
+    show_statistics(metadata)
+
+    write_metadata("../valid_metadata/training_metadata_valid.csv", metadata)
 
     dir_path="../data/train_tsv"
     list_files=[]
@@ -13,13 +19,11 @@ def main():
         if file.endswith(".tsv"):
             list_files.append(file)
 
-    print(len(list_files))
 
-    show_statistics(metadata)
-
-    #for (index, file) in enumerate(list_files):
-    #    print(index, " ", file)
-    #    data = format(file, metadata)
+    if 0:
+        for (index, file) in enumerate(list_files):
+            print(f"{index}/{len(list_files)}: {file}")
+            data = format(file, metadata)
 
 
 
